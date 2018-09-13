dd19c43cec995d757fad59ec969925cc41fc21d9
vita7y <vitaliy.efimenko@filuet.ru>
2018-09-13 10:58:00 +0300

    First commit

---
+++ NUnit.StateBehaviorTest/NUnit.StateBehaviorTest/NUnit.StateBehaviorTest.cs
@@ -0,0 +1,96 @@
+﻿using NUnit.Framework;
+
+namespace NUnit.StateBehaviorTest
+{
+    public static class AssertStateBehavior
+    {
+        public static AssertStateFirst<T> InitializeStateBehavior<T>()
+        {
+            return new AssertStateFirst<T>();
+        }
+
+        public static IAssertState<T> Then<T>(this IAssertStateFirst<T> assertedAssertState, T state, T expectState)
+        {
+            assertedAssertState.CurrentState = state;
+
+            if (assertedAssertState.Next != null) return assertedAssertState.Next;
+
+            Assert.AreEqual(state, expectState);
+            assertedAssertState.Next = new AssertStateNext<T>(state, assertedAssertState);
+            return new AssertStateLast<T>(assertedAssertState);
+        }
+
+        public static IAssertState<T> Then<T>(this IAssertState<T> assertedAssertState, T state, T expectState)
+        {
+            if (assertedAssertState is IAssertStateLast<T>)
+                return assertedAssertState;
+            if (assertedAssertState.Next != null)
+                return assertedAssertState.Next;
+
+            Assert.AreEqual(state, expectState);
+            assertedAssertState.Next = new AssertStateNext<T>(expectState, assertedAssertState.First);
+            return new AssertStateLast<T>(assertedAssertState.First);
+        }
+
+        public static IAssertState<T> Then<T>(this IAssertState<T> assertedAssertState, T expectState)
+        {
+            if (assertedAssertState is IAssertStateLast<T>)
+                return assertedAssertState;
+            if (assertedAssertState.Next != null)
+            {
+                assertedAssertState.Next.CurrentState = assertedAssertState.CurrentState;
+                return assertedAssertState.Next;
+            }
+
+            Assert.AreEqual(assertedAssertState.First.CurrentState, expectState);
+            assertedAssertState.Next = new AssertStateNext<T>(expectState, assertedAssertState.First);
+            return new AssertStateLast<T>(assertedAssertState.First);
+        }
+
+        public interface IAssertStateFirst<T>
+        {
+            IAssertState<T> Next { get; set; }
+            T CurrentState { get; set; }
+        }
+
+        public interface IAssertState<T> : IAssertStateFirst<T>
+        {
+            IAssertStateFirst<T> First { get; }
+        }
+
+        public interface IAssertStateLast<T> : IAssertState<T>
+        {
+        }
+
+        public class AssertStateFirst<T> : IAssertStateFirst<T>
+        {
+            public IAssertState<T> Next { get; set; }
+            public T CurrentState { get; set; }
+        }
+
+        public class AssertStateNext<T> : IAssertState<T>
+        {
+            public AssertStateNext(T state, IAssertStateFirst<T> first)
+            {
+                CurrentState = state;
+                First = first;
+            }
+
+            public IAssertState<T> Next { get; set; }
+            public T CurrentState { get; set; }
+            public IAssertStateFirst<T> First { get; }
+        }
+
+        public class AssertStateLast<T> : IAssertStateLast<T>
+        {
+            public AssertStateLast(IAssertStateFirst<T> first)
+            {
+                First = first;
+            }
+
+            public IAssertState<T> Next { get; set; }
+            public T CurrentState { get; set; }
+            public IAssertStateFirst<T> First { get; }
+        }
+    }
+}
---
+++ NUnit.StateBehaviorTest/NUnit.StateBehaviorTest/TestBehavior.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace NUnit.StateBehaviorTest
+{
+    [TestFixture]
+    public class TestBehavior
+    {
+        private enum TestEnum
+        {
+            First,
+            Second,
+            Fird
+        }
+
+        [Test]
+        public void Then_LongFormForChangeStatuses_Ok()
+        {
+            var ss = AssertStateBehavior.InitializeStateBehavior<TestEnum>();
+            for (var i = 0; i < 3; i++) ss.Then(TestEnum.Fird, TestEnum.Fird).Then(TestEnum.Second, TestEnum.Second).Then(TestEnum.Fird, TestEnum.Fird);
+        }
+
+        [Test]
+        public void Then_LongFormForStatuses_Ok()
+        {
+            var ss = AssertStateBehavior.InitializeStateBehavior<TestEnum>();
+            var list = new List<TestEnum> {TestEnum.First, TestEnum.Second, TestEnum.Fird};
+            for (var i = 0; i < 3; i++)
+            {
+                var res = list[i];
+                ss.Then(res, TestEnum.First).Then(res, TestEnum.Second).Then(res, TestEnum.Fird);
+            }
+        }
+
+        [Test]
+        public void Then_ShortFormChangeStatuses_Ok()
+        {
+            var ss = AssertStateBehavior.InitializeStateBehavior<TestEnum>();
+            var list = new List<TestEnum> {TestEnum.First, TestEnum.Second, TestEnum.Fird};
+            for (var i = 0; i < 3; i++)
+            {
+                var res = list[i];
+                ss.Then(res, TestEnum.First).Then(TestEnum.Fird, TestEnum.Fird).Then(TestEnum.Fird);
+            }
+        }
+
+        [Test]
+        public void Then_ShortFormForStatuses_Ok()
+        {
+            var ss = AssertStateBehavior.InitializeStateBehavior<TestEnum>();
+            var list = new List<TestEnum> {TestEnum.First, TestEnum.Second, TestEnum.Fird};
+            for (var i = 0; i < 3; i++)
+            {
+                var res = list[i];
+                ss.Then(res, TestEnum.First).Then(TestEnum.Second).Then(TestEnum.Fird);
+            }
+        }
+    }
+}
